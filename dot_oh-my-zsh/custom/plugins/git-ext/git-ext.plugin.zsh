fc3dba15ee1ce7e4c7455a1ebdae54873fba507d
Thiago Moreira <tmoreira2020@gmail.com>
2026-07-13 23:12:59 -0300

    Update logic to use none deprecated variable
    Fixes the message "[oh-my-zsh] 'current_branch' is deprecated, using 'git_current_branch' instead."

--- dot_oh-my-zsh/custom/plugins/git-ext/git-ext.plugin.zsh
+++ dot_oh-my-zsh/custom/plugins/git-ext/git-ext.plugin.zsh
@@ -1,8 +1,3 @@
-function current_branch() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo ${ref#refs/heads/}
-}
-
 function roll_back() {
   git diff -p -R "$1^" "$1" > "$1".rollback
 }
@@ -15,16 +10,16 @@ alias ga="gitk --all"
 alias gbc="git checkout -b"
 alias gbd="git branch -D"
 alias gbdlocal="git branch -D"
-alias gbr="git branch -m $(current_branch)"
+alias gbr="git branch -m $(git_current_branch)"
 alias gbsw="git switch"
 alias gconf="cat .git/config"
 alias gd="git diff"
 alias gdl="git diff HEAD~"
 alias gg="git gui"
 alias gp="git push"
-alias gpob="git push origin $(current_branch)"
+alias gpob="git push origin $(git_current_branch)"
 alias gpr="git pull --rebase"
-alias gpub="git push upstream $(current_branch)"
+alias gpub="git push upstream $(git_current_branch)"
 alias gpublish="git publish"
 alias gra="git rebase --abort"
 alias grb="roll_back"
@@ -33,14 +28,14 @@ alias grehard="git reset --hard"
 alias gresoft="git reset --soft"
 alias grm="git rebase master"
 alias grs="git rebase --skip"
-alias gru="git pull --rebase upstream $(current_branch)"
+alias gru="git pull --rebase upstream $(git_current_branch)"
 alias gs="git status"
 alias gsa="git stash apply"
 alias gsl="git stash list"
 alias gsl="git stash list"
 alias gsprout="git sprout"
 alias gss="git stash save"
-alias gsync="git pull --rebase upstream $(current_branch) && git push origin $(current_branch)"
+alias gsync="git pull --rebase upstream $(git_current_branch) && git push origin $(git_current_branch)"
 alias gt="git_tree"
 alias gundo="git reset --soft HEAD~1"
 alias gunpublish="git unpublish"
